364afa47b0ce8439a1158835d83f4071cec022ee
M-Jasim-M <jasimnadan@gmail.com>
2023-07-10 23:43:15 -0700

    this is the login signup with firebase data base

---
+++ firebaselink.js
@@ -0,0 +1,38 @@
+/*const firebaseConfig = {
+    apiKey: "AIzaSyAMMVUjZyp0m34Y1nB6lk46pNXTVEvXbh8",
+    authDomain: "form-f1f8e.firebaseapp.com",
+    projectId: "form-f1f8e",
+    storageBucket: "form-f1f8e.appspot.com",
+    messagingSenderId: "643562271921",
+    appId: "1:643562271921:web:b0aa34ebd04fa9f4fc9e90"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  let db = firbase.firestore();*/
+
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAIRCdhALOL2uLIScamkWa9SQuBMulkF4",
+    authDomain: "furqan-cfc50.firebaseapp.com",
+    projectId: "furqan-cfc50",
+    storageBucket: "furqan-cfc50.appspot.com",
+    messagingSenderId: "1065641461616",
+    appId: "1:1065641461616:web:386130908ebc91b15ef1b5"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); 
+
+let db = firebase.firestore();
+
+//   firebase.initializeApp(firebaseConfig);
+
+//   let db = firebase.firestore();
+
+  
